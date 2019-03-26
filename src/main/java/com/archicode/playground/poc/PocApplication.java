73ef2b68710bccb5b1e31d50925f557cb6addd06
TomaszKoz <tomasz.kozlowski@gmx.com>
2019-03-26 23:01:55 +0100

    Home form

---
+++ src/main/java/com/archicode/playground/poc/PocApplication.java
@@ -0,0 +1,28 @@
+package com.archicode.playground.poc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ * @author Tomasz Kozlowski (created on 22.03.2019)
+ */
+public class PocApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("jfx/Home.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.show();
+    }
+
+}
---
+++ src/main/java/com/archicode/playground/poc/jfx/HomeController.java
@@ -0,0 +1,8 @@
+package com.archicode.playground.poc.jfx;
+
+/**
+ * @author Tomasz Kozlowski (created on 22.03.2019)
+ */
+public class HomeController {
+
+}
